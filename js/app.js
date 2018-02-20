4879c42373e7c2fe2cb754da4f235740bf1c182b
danielvportlandia <danielshelton90@gmail.com>
2018-02-19 20:49:24 -0800

    Object instantiation restructured.

--- js/app.js
+++ js/app.js
@@ -1,70 +1,72 @@
 'use strict';
 
-//image array
-var productOne = document.getElementById('product1');
-var productTwo = document.getElementById('product2');
-var productThree = document.getElementById('product3');
-// var getTarget = event.target;
-var totalClicks = [];
-var totalViews = [];
+//links variables to DOM elements.
+var productOne = document.getElementById('image1');
+var productTwo = document.getElementById('image2');
+var productThree = document.getElementById('image3');
+var imageList = document.getElementById('imageList');
+
+//Instances array
 Product.allProducts = [];
+var names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
-//constructor
-function Product(name, filepath) {
+//Product constructor
+function Product(name) {
   this.name = name;
-  this.totalClicks = totalClicks;
-  this.totalViews = totalViews;
-  this.filepath = filepath;
-
+  this.filepath = 'img/' + name + '.jpg';
+  this.totalClicks = 0;
+  this.totalViews = 0;
   Product.allProducts.push(this);
 }
 
-//instances
-new Product('bag', 'img/bag.jpg');
-new Product('banana', 'img/banana.jpg');
-new Product('bathroom', 'img/bathroom.jpg');
-new Product('boots', 'img/boots.jpg');
-new Product('breakfast', 'img/breakfast.jpg');
-new Product('bubblegum', 'img/bubblegum.jpg');
-new Product('chair', 'img/chair.jpg');
-new Product('cthulhu', 'img/cthulhu.jpg');
-new Product('dog-duck', 'img/dog-duck.jpg');
-new Product('dragon', 'img/dragon.jpg');
-new Product('pen', 'img/pen.jpg');
-new Product('pet-sweep', 'img/pet-sweep.jpg');
-new Product('scissors', 'img/scissors.jpg');
-new Product('shark', 'img/shark.jpg');
-new Product('sweep', 'img/sweep.png');
-new Product('tauntaun', 'img/tauntaun.jpg');
-new Product('unicorn', 'img/unicorn.jpg');
-new Product('usb', 'img/usb.gif');
-new Product('water-can', 'img/water-can.jpg');
-new Product('wine-glass', 'img/wine-glass.jpg');
-
-//randomizer functions
-function randomProductOne() {
-  var randomProductOne = Math.floor(Math.random() * Product.allProducts.length);
-  productOne.src = Product.allProducts[randomProductOne].filepath;
-  productOne.alt = Product.allProducts[randomProductOne].name;
-  productOne.title = Product.allProducts[randomProductOne].name;
+//random number generator
+function getRandomNumber() {
+  return Math.floor(Math.random() * Product.allProducts.length);
 }
 
-function randomProductTwo() {
-  var randomProductTwo = Math.floor(Math.random() * Product.allProducts.length);
-  productTwo.src = Product.allProducts[randomProductTwo].filepath;
-  productTwo.alt = Product.allProducts[randomProductTwo].name;
-  productTwo.title = Product.allProducts[randomProductTwo].name;
+//iterates through all names to generate each instance.
+for (var i = 0; i < names.length; i++) {
+  new Product(names[i]);
 }
 
-function randomProductThree() {
-  var randomProductThree = Math.floor(Math.random() * Product.allProducts.length);
-  productThree.src = Product.allProducts[randomProductThree].filepath;
-  productThree.alt = Product.allProducts[randomProductThree].name;
-  productThree.title = Product.allProducts[randomProductThree].name;
+/* This function displays the images on the page by creating an array to hold each displayed image (3 total), if the displayed image of index 1 matches index 0, it logs it as duplicate and generates a new random number for that index. The displayed image of index 2 does the same thing but compares to the result of both index 0 and 1.
+*/
+function displayImages() {
+  var displayed = [];
+  displayed[0] = getRandomNumber();
+  displayed[1] = getRandomNumber();
+  while (displayed[1] === displayed[0]) {
+    console.log('duplicate image.');
+    displayed[1] = getRandomNumber();
+  }
+  displayed[2] = getRandomNumber();
+  while (displayed[2] === displayed[0] || displayed[2] === displayed[1]) {
+    console.log('duplicate image.');
+    displayed[2] = getRandomNumber();
+  }
+  productOne.src = Product.allProducts[displayed[0]].filepath;
+  productTwo.src = Product.allProducts[displayed[1]].filepath;
+  productThree.src = Product.allProducts[displayed[2]].filepath;
+  productOne.id = Product.allProducts[displayed[0]].name;
+  productTwo.id = Product.allProducts[displayed[1]].name;
+  productThree.id = Product.allProducts[displayed[2]].name;
 }
 
-randomProductOne();
-randomProductTwo();
-randomProductThree();
+//redisplays images once an image is clicked on.
+function calcTotalClicks(e) {
+  console.log(e.target.id);
+  if (e.target.id === 'imageList') {
+    alert('Invalid selection.');
+  }
+  for (var i = 0; i < Product.allProducts.length; i++) {
+    if (e.target.id === Product.allProducts[i].names) {
+      Product.allProducts[i].totalClicks += 1;
+    }
+  }
+  displayImages();
+}
 
+imageList.addEventListener('click', calcTotalClicks);
 
+//initial display of images.
+displayImages();
